090fd54fd42b00901ad5e2037479aea1fe203805
David Alfonso <developer@davidalfonso.es>
2023-01-30 14:42:38 +0100

    2022: day 3

---
+++ day03.py
@@ -0,0 +1,40 @@
+from functools import reduce
+import common as c
+import string
+
+
+def priority(c):
+    if c in string.ascii_lowercase:
+        return ord(c) - ord('a') + 1
+    else:
+        return ord(c) - ord('A') + 27
+
+
+# part 1
+
+def find_duplicated(rs):
+    c1, c2 = rs[:len(rs) // 2], rs[len(rs) // 2:]
+    return (set(c1) & set(c2)).pop()
+
+
+rucksacks = c.strings(c.day(3))
+
+priorities = 0
+for rs in rucksacks:
+    priorities += priority(find_duplicated(rs))
+
+print(priorities)
+
+# part 2
+
+def find_common(rucksacks):
+    rucksack_items = map(set, rucksacks)
+    common_item = reduce(lambda x, y: x & y, rucksack_items)
+    return common_item.pop()
+
+
+priorities = 0
+for group_rucksacks in c.chunked(rucksacks, 3):
+    priorities += priority(find_common(group_rucksacks))
+
+print(priorities)
